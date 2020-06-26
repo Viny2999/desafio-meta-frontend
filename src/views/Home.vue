61969681fbfa54084e191111dad92705bfb4fbf5
Vinicius Menezes <viny2999@gmail.com>
2020-06-26 20:44:28 -0300

    use router

---
+++ src/views/Home.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar
+      app
+      clipped-right
+      color="blue-grey"
+      dark
+    >
+      <!--<v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>-->
+      <v-toolbar-title>Toolbar</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-icon @click.stop="right = !right">mdi-exit-to-app</v-icon>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="left"
+      fixed
+      temporary
+    ></v-navigation-drawer>
+
+    <v-main>
+     <!--Here-->
+    </v-main>
+
+    <v-navigation-drawer
+      v-model="right"
+      fixed
+      right
+      temporary
+      width=450
+    >
+    <v-icon @click.stop="right = !right">mdi-close</v-icon>
+    </v-navigation-drawer>
+
+    <v-footer
+      app
+      color="blue-grey"
+      class="white--text"
+    >
+      <span>No Ar</span>
+      <v-spacer></v-spacer>
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    props: {
+      source: String,
+    },
+
+    data: () => ({
+      drawer: null,
+      drawerRight: null,
+      right: false,
+      left: false,
+    }),
+  }
+</script>
