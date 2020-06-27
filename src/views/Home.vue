b0fd723d7a30f9736bb785f0c85472d64624f3e3
Vinicius Menezes <viny2999@gmail.com>
2020-06-26 23:33:20 -0300

    use sass

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,10 +3,11 @@
     <v-app-bar
       app
       clipped-right
-      color="blue-grey"
+      color="grey lighten-5"
       dark
+      height=90
     >
-      <!--<v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>-->
+      <button class="button-left">Oi</button>
       <v-toolbar-title>Toolbar</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-icon @click.stop="right = !right">mdi-exit-to-app</v-icon>
@@ -23,7 +24,16 @@
       temporary
       width=450
     >
-    <v-icon @click.stop="right = !right">mdi-close</v-icon>
+      <v-app-bar
+        app
+        clipped-right
+        color="grey lighten-5"
+        dark
+        height=90
+      >
+        Filtros
+        <v-icon @click.stop="right = !right">mdi-close</v-icon>
+      </v-app-bar>
     </v-navigation-drawer>
 
     <Footer />
@@ -42,3 +52,11 @@
     }),
   }
 </script>
+
+<style lang="sass">
+  .button-left
+    background: linear-gradient(to top right, #ff0066 -2%, #ff6600 100%)
+    border-radius: 30px/10px
+    padding: 11px 21px
+    margin-left: 20px
+</style>
